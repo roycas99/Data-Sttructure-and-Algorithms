50880b9e6cd095d1a2490fea37464aa62d103494
roycas99 <jamesblack1319@gmail.com>
2022-05-21 16:20:42 -0500

    Review for Java Array

---
+++ Basics class/src/ArrayIsCool.java
@@ -0,0 +1,17 @@
+
+public class ArrayIsCool {
+
+	public static void main(String[] args) {
+		// declare array
+		
+		int[] prime= {1,3,7};
+		
+		// how to access array
+		System.out.println(prime[0]);
+		
+		
+		
+
+	}
+
+}
